5d445a4a210335b89b2289bb2642431181f01495
andru17urdna <dr3wwatkins@gmail.com>
2022-01-12 14:58:26 -0600

    Added iteration through top headlines

--- components/TopHeadlines.vue
+++ components/TopHeadlines.vue
@@ -1,7 +1,16 @@
 <template>
     <div>
         <h1>TOP-HEADLINES</h1>
-        <span>{{headlines}}</span>
+        <Article v-if="topHeadline !== null" :source="topHeadline.source" :author="topHeadline.source"
+             :content="topHeadline.content" :description="topHeadline.description" :publishedAt="topHeadline.publishedAt"
+             :title="topHeadline.title" :url="topHeadline.url" :urlToImage="topHeadline.urlToImage"/>
+            <!-- <p>{{topHeadline.source}}</p>
+            <p>{{topHeadline.author}}</p>
+            <p>{{topHeadline.title}}</p>
+            <p>{{topHeadline.description}}</p>
+            <p>{{topHeadline.url}}</p>
+            <p>{{topHeadline.urlToImage}}</p>
+            <p>{{topHeadline.content}}</p> -->
     </div>
 </template>
 
@@ -13,6 +22,8 @@ export default {
     data() {
         return {
             headlines: [],
+            topHeadline: null,
+            interval: null,
         }
     },
     async created() {
@@ -26,10 +37,20 @@ export default {
             const res = await axios.get(`http://localhost:8000/api/news/top-headlines`, config);
             console.log(res.data)
             this.headlines = res.data.articles
-
-            } catch (err) {
+            this.iterate();
+        } catch (err) {
             console.log(err);
-            }
+        }
+    },
+    methods: {
+        iterate() {
+            let num = 0;
+            this.interval = setInterval(() => {
+                this.topHeadline = this.headlines[num];
+                num++
+                if (num === this.headlines.length) num = 0;
+            }, 3000)
+        }
     }
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
-  <!-- <Tutorial /> -->
   <TopHeadlines />
+  <Tutorial />
 </div>
 </template>
 
