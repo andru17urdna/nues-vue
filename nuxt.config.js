3b45fe64ba4a58097036b723b16e787846cee4ce
andru17urdna <dr3wwatkins@gmail.com>
2022-01-15 15:03:16 -0600

    -Added Simple Meta-tags for overall project

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,15 +1,17 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'your-news',
+    title: 'Nues with Vue',
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { name: 'author', content: 'Andru' },
       { name: 'format-detection', content: 'telephone=no' },
+      { name: 'keywords', content: 'HTML, CSS, JavaScript, Vue, Nuxt' },
+      { hid: 'description', name: 'description', content: 'News available from every source, all in one place.' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
